459306522be389451ca01f10578bd1e14925425c
liqilong <liqilong199126@gmail.com>
2020-01-20 15:34:54 +0800

    面向对象--继承

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
-class Person {
-    constructor(name) {
+class People {
+    constructor(name, age) {
         this.name = name
+        this.age = age
     }
-    getName() {
-        return this.name
+    eat() {
+        alert(`${this.name} eat something`)
+    }
+    speak() {
+        alert(`My name is ${this.name}, age ${this.age}`)
     }
 }
 
-let p = new Person('liqilong')
-alert(p.getName())
+class Student extends People {
+    constructor(name, age, number) {
+        super(name, age)
+        this.number = number
+    }
+    study() {
+        alert(`${this.name} study`)
+    }
+}
+
+let xiaoming = new Student('xiaomimg', 10, 'A1')
+xiaoming.study()
+alert(xiaoming.number)
+xiaoming.eat()
+
+let xiaohong = new Student('xiaohong', 11, 'A2')
+xiaohong.study()
+xiaohong.speak()
